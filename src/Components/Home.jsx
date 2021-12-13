c93a1bffe7da37ece4531946f2c8b11cd2876a5b
marshWhale <39956222+kjb0787@users.noreply.github.com>
2021-12-12 21:23:36 -0800

    added search page and redirect from home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,33 @@
 import { Navigation } from "./Navigation";
+import { useNavigate } from 'react-router';
+import React, { useState } from "react";
+import './Home.css';
 
 export default function Home() {
+    const [formInput, setFormInput] = useState('');
+    const [errorMsg, setError] = useState(null);
+    const navigate = useNavigate();
+
+    function onSearchButtonClick() {
+        navigate("/search");
+    }
+
     return (
         <div>
             <Navigation />
+            <div class="searchContainer">
+                <div class="searchSection">
+                    {errorMsg}
+                    <input type='text' value={formInput}
+                        onChange={(e) => {
+                            setError(null);
+                            setFormInput(e.target.value)
+                        }} />
+                    <button class="glow-on-hover" onClick={onSearchButtonClick}>
+                        Search for Job
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,9 +1,57 @@
 import { Navigation } from "./Navigation";
+import { useState } from 'react';
+import axios from 'axios';
+import './Search.css';
 
 export default function Search() {
+    const [formInput, setFormInput] = useState('');
+    const [job, setJob] = useState({
+      title: "", 
+      location: "",
+      companyName: ""
+    })
+    const [errorMsg, setError] = useState(null);
+  
+    function onSearchButtonClick() {      
+      if (!formInput) {
+        setError("You must type in a job title.");
+        return;
+      }
+  
+      axios.get('/api/job/' + formInput)
+        .then(response => setJob(response.data))
+        .catch(error => setJob({
+            title: 'No such job found', 
+            location: "N/A",
+            companyName: "N/A"
+        }));
+    }
+
     return (
         <div>
             <Navigation />
+            <div>
+                {errorMsg}
+                <input type='text' value={formInput}
+                    onChange={(e) => {
+                        setError(null);
+                        setFormInput(e.target.value)
+                    }} />
+                <button onClick={onSearchButtonClick}>
+                    Search for Job
+                </button>
+                <div class="searchResult">
+                <div>
+                    Job Title: {job.title}
+                </div>
+                <div>
+                    Location: {job.location}
+                </div>
+                <div>
+                    Company Name: {job.companyName}
+                </div>
+                </div>
+            </div>
         </div>
     );
 }
